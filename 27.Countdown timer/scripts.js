54b33778f11ad432680ec9d6d7f2d1128b9fead9
Ananth Bhat <anant@Vivobook.S15>
2018-10-21 15:52:51 +0530

    added countdown timer

---
+++ 27.Countdown timer/scripts.js
@@ -0,0 +1,14 @@
+let countdown;
+
+function timer(seconds) {
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    countdown = setInterval(() => {
+        secondsLeft = Math.round((then - Date.now()) / 1000);
+        if (secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
+        console.log(secondsLeft);
+    }, 1000);
+}
